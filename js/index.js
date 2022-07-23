1ac076640c357ba615a705bcb7740d4bce8fd40b
luiz <luiz.contes@hotmail.com>
2022-07-23 03:28:32 +0100

    Side bar added, layout changed
    I included a side bar and at the center there is going to be projects previews, also the introduction was inserted in a lightbox card

--- js/index.js
+++ js/index.js
@@ -4,7 +4,39 @@ let counter = 0;
 
 function icon() {
     favicon.href = favicons[counter];
-    counter = (counter+1) % 4;
+    counter = (counter + 1) % 4;
 }
 
 setInterval(icon, 300);
+
+
+const lightbox = document.getElementById('lightbox');
+const logo = document.getElementById('logo')
+logo.addEventListener('click', () => turnLightbox())
+
+const turnLightbox = () => {
+    lightbox.classList.toggle('lightbox-hidden');
+    lightbox.classList.toggle('lightbox');
+}
+
+const body = document.getElementById('body');
+body.addEventListener('click', (e) => {
+    // console.log(e.target)
+    if (e.target.matches('#close') || e.target.matches('#lightbox')) {
+        lightbox.classList.toggle('lightbox-out');
+        setTimeout(() => {
+            lightbox.classList.toggle('lightbox-out');
+            lightbox.classList.toggle('lightbox-hidden');
+            lightbox.classList.toggle('lightbox')
+        }, 1000);
+    }
+});
+
+const sideMenu = document.getElementById('side-menu')
+const menuBtn = document.querySelector('.menu-btn')
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open')
+    sideMenu.classList.toggle('active')
+})
+
+setTimeout(turnLightbox, 1000);
